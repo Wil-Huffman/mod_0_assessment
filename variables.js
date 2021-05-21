94c2135bff1e363e327c3ecefb72f23f38f93928
Wil-Huffman <me@wil-huffman.io>
2021-05-21 12:55:39 -0500

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var animal = "otter";
+var isCuddly = true;
+animal = "oyster";
+var num8 = 8;
+var num3 = 2;
+var quotient = num8 / num3;
+var favoriteFilms = ["Koyaanisqatsi", "Russian Arc", "Mean Girls"];
+shift(0);
+var animalLength = animal.length;
