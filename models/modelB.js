6ebc1be56adae63348208c96798789a17f76bcc6
Wil-Huffman <me@wil-huffman.io>
2021-05-21 12:16:06 -0500

    Add Object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var custom_Home = {
+  location: "oklahoma city",
+  neighborhood: "the plaza",
+  askingPrice: 225000,
+  numberOfBathrooms: 2.5,
+  isNewBuild: true,
+  builtByReputableBuilder: true,
+  extras: ["security system", "heated bathroom floors", "recovered barn wood flooring"],
+  neighborNames: ["bob and sheila", "peggy and lou", "frank the tank"],
+}
