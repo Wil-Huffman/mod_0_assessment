eb021f3a63b133982b45906be3e18871751468f3
Wil-Huffman <me@wil-huffman.io>
2021-05-23 13:30:39 -0500

    Change Formatting

--- models/modelA.js
+++ models/modelA.js
@@ -1,10 +1,10 @@
 var laptop = {
-  manufacturer : "apple",
-  model : "macbook",
-  cores : 8,
-  cost : 2100,
-  isFast : true,
-  hasBlackCase : false,
-  upgrades : ['applecare', 'retina display', '1 terrabyte hd'],
-  softwareFeatures : ['os x 11.2', 'garage band included', 'free trial of audio recording program'],
+  manufacturer: "apple",
+  model: "macbook",
+  cores: 8,
+  cost: 2100,
+  isFast: true,
+  hasBlackCase: false,
+  upgrades: ['applecare', 'retina display', '1 terrabyte hd'],
+  softwareFeatures: ['os x 11.2', 'garage band included', 'free trial of audio recording program'],
 }
